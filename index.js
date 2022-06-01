9cf37ae9cd2fdad647a887f9fca5803606758fec
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-01 19:48:22 +0300

    added functions to index.js file

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(hello) {
+    return hello.toUpperCase();
+    
+}
+function whisper(HELLO){
+    return HELLO.toLowerCase();
+}
+function logShout(fire){
+    console.log(fire.toUpperCase());
+}
+function logWhisper(cold){
+    console.log(cold.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(hello) {
+  if(hello === "hello"){
+      return "I can't hear you!"
+  }
+  else if(hello === "HELLO"){
+      return "YES INDEED!"
+  }
+  else if(hello === "Let's have dinner together!"){
+    return "I would love to!"
+}
+  
+}
+
+  
